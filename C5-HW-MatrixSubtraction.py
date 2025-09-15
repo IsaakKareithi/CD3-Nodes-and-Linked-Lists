8e342ec3262ae5e201cf90c20ba6c957fd9ebcb8
isaakKareithi <isaacsgk2@gmail.com>
2025-09-15 18:10:38 +0300

    C5-HW-MatrixSubtraction.py

---
+++ C5-HW-MatrixSubtraction.py
@@ -0,0 +1,15 @@
+#initialize matrix
+
+x = [[11,9],[2,4]]
+
+y = [[7,5],[1,5]]
+
+answer = [[0,0],[0,0]]
+
+#traverse array 
+for i in range(len(x)):
+    for j in range(len(y[0])):
+        answer[i][j] = x[i][j] - y[i][j]
+
+for r in answer:
+    print(r)
